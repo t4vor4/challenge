44692f9b7e334765afe6a5c8ef9a9e2a09da14fe
Fernando <fernando@agenciaeplus.com.br>
2019-07-01 20:31:09 -0300

    Finalizando o projeto com o arquivo readme

--- README.md
+++ README.md
@@ -1,33 +1,11 @@
-# Quero trabalhar na Boticário: Front-End
-Desenvolver uma aplicação HTML5 + CSS3 + JS
+# Teste front-end
+Teste de página desenvolvida utilizando HTML5 + CSS3 + JS e o pré-processador Gulp
 
 ## Instruções
-- Faça um fork desse projeto para a sua conta pessoal do GitHub, ou BitBucket.
-- Siga as especificações abaixo.
-- Crie um README com as instruções para compilar, testar e rodar o projeto.
-- O link do repositório deverá ser enviado para o e-mail felipe.dasilva@grupoboticario.com.br com o título **Teste Front-End**
+- Baixe ou clone este repositório.
+- Entre na pasta do projeto (/challenge)
+- Digite 'npm install' para instalar as dependencias na pasta do projeto'.
+- Terminada a instalação das dependencias, digite 'npm start' para iniciar o ambiente de desenvolvimento.
+- Para o ambiente de produção, digite no terminal 'npm run-script build'
 
-## Especificações técnicas
-- Utilizar HTML5, CSS3 e JS (Vanilla, jQuery, Vue ou React... Fica a seu critério)
-- Mobile first e responsivo
-
-## A missão
-Desenvolver uma página, baseando-se no wireframe abaixo (PSD e PNG na pasta assets):
-![boticario](assets/eCommerce-Template.png)
-
-Hover mini cart:
-![boticario](assets/hover-eCommerce-PSD-Template.png)
-
-- Os produtos precisam ser consumidos de um .json (está na pasta assets) via ajax.
-- Ao clicar no produto, o mesmo deve ser adicionado ao carrinho.
-- Último e não menos importante, deixe sua imaginação fluir.
-
-## O que será avaliado?
-- Semântica Web
-- Organização do código
-- Lógica do código
-- Uso do Git
-- Documentação
-- Automação de tarefas (Gulp? Grunt? Webpack? :D)
-
-Acreditamos que 1 (um) dia, é um tempo legal para resolver o desafio proposto, mas sabemos que nem todo mundo tem o mesmo nível de disponibilidade. Portanto, nos avise se precisar de mais tempo ;)
+:)
--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,27 @@
 function filtraJson() {
     let html = ''
     const url = './assets/products.json'
+    const lista = [];
     fetch(url)
         .then(resp => resp.json())
         .then(obj => {
             $(obj).each((i,el) => {
+                lista.push(el)
                 const img = el.images[0].imageUrl;
                 const nome = el.name
                 const valor = el.Value.toString().replace('.',',')
-                console.log()
-                html = html+`<li><img src="${img}" alt="Imagem do produto ${nome}" />
+            
+                html = html+`<li id="${i}" value="${el.Value}" price="R$ ${valor}" title="${nome}"><img src="${img}" alt="Imagem do produto ${nome}" />
                 <div>
-                    <span class="nome" title="${nome}">${nome}</span>
+                    <span class="nome">${nome}</span>
                     <span class="preco">R$ ${valor}</span>
                 </div></li>` 
             })
         })
         .then( _ => {
-            // console.log(html)
-            $('[vitrinecontent]').append(html)
-
+            $('[vitrinecontent]').append(html);
         })
+        .then(addAoCarrinho)
 }
 
 function menuMobile() {
@@ -36,6 +37,59 @@ function menuMobile() {
     })
 }
 
+
+function addAoCarrinho() {
+    
+    const cart = []
+
+    const montaCarrinho = (arr) => {
+        let html = ''
+        $(arr).each((i,el) => {
+            html = html + `<li class="listcart" id="${el.id}">
+            <img src="${el.img}"/>
+            <span class="nome">${el.nome}</span>
+            <span class="qtd">1 <small>itens</small></span>
+            <span class="preco">R$ ${el.price}</span>
+            </li>`;
+        })
+
+        return html        
+    }
+
+    const avisoCarrinho = _ => {
+        $('.smallcall').addClass('show')
+        setTimeout( _ => $('.smallcall').removeClass('show'), 2000);
+    }
+
+    const montaValorAtual = (arr) => {
+        const values = arr.map((e) => parseFloat(e.value))
+        const soma = (ac,cu) => (ac*1)+(cu*1)
+        const total = values.reduce(soma,'').toFixed(2).replace('.',',')
+        return total        
+    }
+
+    
+
+    $('.vitrine_content li').each((i,el) => {
+        $(el).click((e) => {
+            const obj  = {
+                img: $(el).find('img').attr('src'),
+                id: $(el).attr('id'),
+                nome: $(el).attr('title'),
+                price: $(el).attr('price'),
+                value: $(el).attr('value')
+            }
+            cart.push(obj)
+            
+            const neoCart = montaCarrinho(cart);
+
+            $('.cart__valor').text(`R$ ${montaValorAtual(cart)}`)         
+            $('.cartitens').html(`<ul>${neoCart}</ul>`);
+            avisoCarrinho();
+
+        })
+    })
+}
 
 
 
